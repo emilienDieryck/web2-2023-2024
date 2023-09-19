e9be6d93e4bcf8cd0a311dd88230e56e913b7b58
EmilienD <emilien.dieryck@student.vinci.be>
2023-09-19 09:16:38 +0200

    exo 1.2

---
+++ exercices/1.2/myMovies/api/basic/routes/films.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+
+var compt = 0;
+
+const films = [
+    {
+        id: 1,
+        title: "Killers of the Flower Moon (2023)",
+        duration: 206,
+        budget: 500000,
+        link: "https://www.imdb.com/title/tt5537002/?ref_=vp_vi_tt" ,
+    },
+    {
+        id: 2,
+        title: "Aquaman et le Royaume perdu (2023)",
+        duration: 180,
+        budget: 1000000,
+        link: "https://www.imdb.com/title/tt9663764/?ref_=vp_vi_tt",
+    },
+    {
+        id: 3,
+        title: "'A Haunting in Venice' (2023)",
+        duration: 190,
+        budget: 675000,
+        link: "https://www.imdb.com/title/tt29081468/?ref_=vp_vi_tt",
+    }
+    ];
+
+router.get('/', (req, res, next)=> {
+    if(req.path){
+        compt = compt+1;
+    }
+    console.log('GET /films'+ ' : ' + compt);
+    res.json(films);
+});
+
+module.exports = router;
