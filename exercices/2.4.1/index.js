eed243c0efb00181b5c8a99f97aad936076c7230
EmilienD <emilien.dieryck@student.vinci.be>
2023-10-16 08:14:47 +0200

    2.4.1 : run action within timer

--- exercices/2.4.1/index.js
+++ exercices/2.4.1/index.js
@@ -6,18 +6,24 @@ const delayInSeconds = 5;
 const delayInMiliSeconds = delayInSeconds * 1000;
 let cmpt = 0;
 
-btn.addEventListener('click' , () => {
-  timeoutID = setTimeout(() => {
+btn.addEventListener('click', start);
 
+function start() { 
+  timeoutID = setTimeout(() => {
+    if(cmpt < 10){
+      message.textContent = 'Game over, you did not click 10 times within 5s !';
+    } 
   }, delayInMiliSeconds);
+  btn.addEventListener('click', btnClick);
+  
+}
+
+function btnClick(){
   cmpt++;
-  if(timeoutID == delayInMiliSeconds && cmpt < 10){
-    message.textContent = 'Game over, you did not click 10 times within 5s !';
+  if(cmpt == 10){
+    message.textContent = `You win ! You clicked 10 times within ${delayInMiliSeconds} ms`;
   }
-  if(timeoutID <= delayInMiliSeconds && cmpt == 10){
-    message.textContent = 'You win ! You clicked 10 times within ${timeoutID} ms';
-  }
-})
+}
 
 function startTimeOut() {
     timeoutID = setTimeout(() => {
